4028ad7e750ecf0062d702d940550dc031578dbb
danielmogos13 <danielmogos13@gmail.com>
2019-06-11 10:45:08 +0300

    do some minor refactoring

--- src/app/modules/app-login/app-login.service.ts
+++ src/app/modules/app-login/app-login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
-import {auth} from 'firebase';
+import {auth} from 'firebase/app';
 import {AngularFirestore} from '@angular/fire/firestore';
 
 
